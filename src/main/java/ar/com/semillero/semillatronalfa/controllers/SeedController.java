acd32af91dab791c66258ff514c85c5c45597263
MartinFiorde <fiorde_90@hotmail.com>
2023-05-27 18:11:47 -0300

    CREATE y READ basico de semillas ok con postman

--- src/main/java/ar/com/semillero/semillatronalfa/controllers/SeedController.java
+++ src/main/java/ar/com/semillero/semillatronalfa/controllers/SeedController.java
@@ -1,46 +1,45 @@
-//package ar.com.semillero.semillatronalfa.controllers;
-//
-//import ar.com.semillero.semillatronalfa.entities.Event;
-//import ar.com.semillero.semillatronalfa.entities.seed.Seed;
-//import ar.com.semillero.semillatronalfa.services.event.EventService;
-//import ar.com.semillero.semillatronalfa.services.seed.SeedService;
-//import java.util.List;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//@Controller
-//@RequestMapping("/seed")
-//public class SeedController {
-//    
-//    private SeedService seedService;
-//
-//    @Autowired
-//    public SeedController(SeedService seedService) {
-//        this.seedService = seedService;
-//    }
-//    
-//    @GetMapping("/{id}")
-//    @ResponseBody
-//    public Seed findEventById(@PathVariable String id) {
-//        return seedService.findSeedById(id);
-//    }
-//
-//    @GetMapping("/list")
-//    @ResponseBody
-//    public List<Seed> findEvents() {
-//        return seedService.findAllSeeds();
-//    }
-//
-//    @PostMapping("/create")
-//    @ResponseBody
-//    public void createSeed(@RequestBody Seed seed) {
-//        seedService.addSeed(seed);
-//    }
-//}
+package ar.com.semillero.semillatronalfa.controllers;
+
+import ar.com.semillero.semillatronalfa.entities.seed.Seed;
+import ar.com.semillero.semillatronalfa.services.seed.SeedService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller // Diferencia controller vs restcontroller: https://medium.com/javarevisited/difference-between-controller-and-restcontroller-in-spring-boot-and-spring-mvc-216578ad445f
+@RequestMapping("/seed")
+public class SeedController {
+    
+    private SeedService seedService;
+
+    @Autowired
+    public SeedController(SeedService seedService) {
+        this.seedService = seedService;
+    }
+    
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Seed findEventById(@PathVariable String id) {
+        return seedService.findSeedById(id);
+    }
+
+    @GetMapping("/list")
+    @ResponseBody
+    public List<Seed> findEvents() {
+        return seedService.findAllSeeds();
+    }
+
+    @PostMapping("/create")
+    @ResponseBody
+    public void createSeed(@RequestBody Seed seed) {
+        System.out.println(seed);
+        seedService.addSeed(seed);
+    }
+}
 
