848df1d2e8f05f38d4304e37e1874f175e478998
Luis-Alberto-Espinoza <espinoza.luis.alberto1981@gmail.com>
2023-05-28 13:21:09 -0300

    renombrado de paq y modificaciones en entidad proyecto

--- src/main/java/ar/com/semillero/semillatronalfa/repositories/project/ProjectDataRepository.java
+++ src/main/java/ar/com/semillero/semillatronalfa/repositories/project/ProjectDataRepository.java
@@ -1,8 +1,8 @@
-package ar.com.semillero.semillatronalfa.repositories.Proyect;
+package ar.com.semillero.semillatronalfa.repositories.project;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import ar.com.semillero.semillatronalfa.entities.proyect.Project;
+import ar.com.semillero.semillatronalfa.entities.project.Project;
 
 public interface ProjectDataRepository extends JpaRepository<Project, String> {
 
