d8682d11e72cc538b9dde1fb769cf212c005b818
MartinFiorde <fiorde_90@hotmail.com>
2023-06-08 18:58:03 -0300

    controlador get de login funcionando, controlado con lucas

--- src/main/java/ar/com/semillero/semillatronalfa/controllers/LoginController.java
+++ src/main/java/ar/com/semillero/semillatronalfa/controllers/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -23,7 +24,7 @@ public class LoginController {
 
     @GetMapping
     //@ResponseBody
-    public String authenticateUser(@RequestBody User user) {
+    public String authenticateUser() {
         return "pages/login.html";
     }
 
