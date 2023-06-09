ece5e546dca62205ecd7eaf12353314180d25aa3
MartinFiorde <fiorde_90@hotmail.com>
2023-06-09 15:17:12 -0300

    controladores para visualizar semilla individual y listado de semillas conectados con html y 100% funcionales

--- src/main/java/ar/com/semillero/semillatronalfa/controllers/SeedController.java
+++ src/main/java/ar/com/semillero/semillatronalfa/controllers/SeedController.java
@@ -3,13 +3,17 @@ package ar.com.semillero.semillatronalfa.controllers;
 import ar.com.semillero.semillatronalfa.entities.seed.Seed;
 import ar.com.semillero.semillatronalfa.services.seed.SeedService;
 import java.util.List;
+import org.hibernate.service.spi.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
@@ -29,20 +33,41 @@ public class SeedController {
     public Seed findEventById(@PathVariable String id) {
         return seedService.findSeedById(id);
     }
-
-    @GetMapping("/list")
+    
+    @GetMapping("/api/item/{id}")
     @ResponseBody
-    public List<Seed> findEvents() {
+    public Seed findSeedById(@PathVariable String id) {
+        return seedService.findSeedById(id);
+    }
+
+    @GetMapping("/item/{id}")
+    public String mostrarLibro(ModelMap model, @PathVariable String id) throws ServiceException {
+        if (id != null) {
+            model.put("id", id);
+            model.put("seed", seedService.findSeedById(id));
+        }
+        return "pages/semilla.html";
+    }
+    
+    @GetMapping("/api/list")
+    @ResponseBody
+    public List<Seed> findAllSeeds() {
         return seedService.findAllSeeds();
     }
 
-    @PostMapping("/create")
+    @GetMapping("/list")
+    public String findSeedHtml(ModelMap model) {
+        model.addAttribute("seeds", seedService.findAllSeeds());
+        return "pages/semillas.html";
+    }
+    
+    @PostMapping("/api/create")
     @ResponseBody
     public void createSeed(@RequestBody Seed seed) {
         seedService.addSeed(seed);
     }
     
-    @PostMapping("/create-batch")
+    @PostMapping("/api/create-batch")
     @ResponseBody
     public void createSeed(@RequestBody List<Seed> seeds) {
         seedService.addSeeds(seeds);
