aed35a0697509d6f94604f5c4d8db1dab0b4a0ed
Luca <dev.luccasassa@gmail.com>
2023-06-09 22:47:20 -0300

    tabs "semilla.html" rusticos

--- src/main/resources/static/js/semilla.js
+++ src/main/resources/static/js/semilla.js
@@ -6,36 +6,54 @@ d.querySelector('.observaciones-menu').addEventListener("click", () => {
   d.querySelector('.observaciones-form-container').classList.toggle("active");
 });
 
-/* d.querySelectorAll(".nav-item").forEach((n) =>
-  n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-  })
-); */
-/* ADAPTAR ESTO A CADA BOTON */
 
 
-/* tabs naranjas */
-d.querySelector('.button-datos-personales').addEventListener("click", () => {
-  d.querySelector('.button-datos-personales').classList.toggle("active");
-  d.querySelector('.tab-datos-personales').classList.toggle("active");
+
+
+
+
+/* tabs */
+const buttonDP = d.querySelector('.button-datos-personales');
+const tabDP = d.querySelector('.tab-datos-personales');
+const buttonDDC = d.querySelector('.button-datos-de-contacto');
+const tabDDC = d.querySelector('.tab-datos-de-contacto');
+const buttonME = d.querySelector('.button-modificar-estado');
+const tabME = d.querySelector('.tab-modificar-estado');
+const buttonAP = d.querySelector('.button-asignar-proyecto');
+const tabAP = d.querySelector('.tab-asignar-proyecto');
+const buttonC = d.querySelector('.button-certificado');
+const tabC =  d.querySelector('.tab-certificado');
+const buttonR = d.querySelector('.button-recomendada');
+const tabR = d.querySelector('.tab-recomendada');
+
+buttonDP.addEventListener("click", () => {
+  buttonDP.classList.toggle("active");
+  tabDP.classList.toggle("active");
+
+  buttonDDC.classList.remove("active");
+  tabDDC.classList.remove("active");
+  buttonME.classList.remove("active");
+  tabME.classList.remove("active");
+  buttonAP.classList.remove("active");
+  tabAP.classList.remove("active");
+  buttonC.classList.remove("active");
+  tabC.classList.remove("active");
+  buttonR.classList.remove("active");
+  tabR.classList.remove("active");
 });
 
-d.querySelector('.button-datos-de-contacto').addEventListener("click", () => {
-  d.querySelector('.button-datos-de-contacto').classList.toggle("active");
-  d.querySelector('.tab-datos-de-contacto').classList.toggle("active");
+buttonDDC.addEventListener("click", () => {
+  buttonDDC.classList.toggle("active");
+  tabDDC.classList.toggle("active");
+
+  buttonDP.classList.remove("active");
+  tabDP.classList.remove("active");
+  buttonME.classList.remove("active");
+  tabME.classList.remove("active");
+  buttonAP.classList.remove("active");
+  tabAP.classList.remove("active");
+  buttonC.classList.remove("active");
+  tabC.classList.remove("active");
+  buttonR.classList.remove("active");
+  tabR.classList.remove("active");
 });
-
-/* d.querySelectorAll(".itemA").forEach((n) =>
-  n.addEventListener("click", () => {
-
-    d.querySelectorAll(".bA").classList.remove("active");
-    d.querySelectorAll(".iA").classList.remove("active");
-    d.querySelectorAll(".a").classList.remove("active");
-    d.querySelectorAll(".b").classList.remove("active");
-    d.querySelectorAll(".c").classList.remove("active");
-    d.querySelectorAll(".d").classList.remove("active");
-    d.querySelectorAll(".e").classList.remove("active");
-    d.querySelectorAll(".f").classList.remove("active");
-  })
-); */
