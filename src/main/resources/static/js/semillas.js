b314920ca0a8609bc460c913641f21f33dedef1d
Luca <dev.luccasassa@gmail.com>
2023-06-11 21:17:43 -0300

    visualizacion de semillas funcionando
    Se agregó paginación de a 40 semillas por vista.
    Estilos arreglados.
    Falta completar la tabla con el resto de datos (firstName y lastName ya están)

--- src/main/resources/static/js/semillas.js
+++ src/main/resources/static/js/semillas.js
@@ -1,28 +1,38 @@
-$(document).ready(function () {
-  $('#tabla-semillas').DataTable({
-      language: {
-          processing: "Tratamiento en curso...",
-          search: "Buscar&nbsp;:",
-          lengthMenu: "Mostrar&nbsp; _MENU_ &nbsp;semillas",
-          info: "Semillas desde _START_ hasta _END_ de _TOTAL_",
-          infoEmpty: "No existen datos.",
-          infoFiltered: "(filtrado de _MAX_ elementos en total)",
-          infoPostFix: "",
-          loadingRecords: "Cargando...",
-          zeroRecords: "No se encontraron datos con tu busqueda",
-          emptyTable: "No hay datos disponibles en la tabla.",
-          paginate: {
-              first: "Primero",
-              previous: "<",
-              next: ">",
-              last: "Ultimo"
-          },
-          aria: {
-              sortAscending: ": active para ordenar la columna en orden ascendente",
-              sortDescending: ": active para ordenar la columna en orden descendente"
-          }
-      },
-      scrollY: 400,
-      lengthMenu: [ [20, 40, 60, 100, -1], [20, 40, 60, 100, "todas"] ],
-  });
-});
+/* TABLE PAGINATION */
+const d = document;
+
+let $table = d.getElementById("tabla-semillas"),
+$n = 40,
+$rowCount = $table.rows.length,
+$firstRow = $table.rows[0].firstElementChild.tagName,
+$hasHead = ($firstRow === "TH"),
+$tr = [],
+$i,$ii,$j = ($hasHead)?1:0,
+$th = ($hasHead?$table.rows[(0)].outerHTML:"");
+
+let $pageCount = Math.ceil($rowCount / $n);
+if ($pageCount > 1) {
+	for ($i = $j,$ii = 0; $i < $rowCount; $i++, $ii++)
+		$tr[$ii] = $table.rows[$i].outerHTML;
+	$table.insertAdjacentHTML("afterend","<div id='pagination'></div");
+	sort(1);
+}
+
+function sort($p) {
+	let $rows = $th,$s = (($n * $p)-$n);
+	for ($i = $s; $i < ($s+$n) && $i < $tr.length; $i++)
+		$rows += $tr[$i];
+	$table.innerHTML = $rows;
+	d.getElementById("pagination").innerHTML = pageButtons($pageCount,$p);
+	d.getElementById("id"+$p).setAttribute("class","active");
+}
+
+function pageButtons($pCount,$cur) {
+	var	$prevDis = ($cur == 1)?"disabled":"",
+		$nextDis = ($cur == $pCount)?"disabled":"",
+		$pagination = "<input id='prev' type='button' value='🡨 Anterior' onclick='sort("+($cur - 1)+")' "+$prevDis+">";
+	for ($i=1; $i<=$pCount;$i++)
+		$pagination += "<input type='button' id='id"+$i+"'value='"+$i+"' onclick='sort("+$i+")'>";
+	$pagination += "<input id='next' type='button' value='Siguiente 🡪' onclick='sort("+($cur + 1)+")' "+$nextDis+">";
+	return $pagination;
+}
