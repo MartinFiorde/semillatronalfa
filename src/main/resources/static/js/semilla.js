8ccf6f0c1a009401025fc3fe871c26e3e9133cef
Luca <dev.luccasassa@gmail.com>
2023-06-12 20:55:44 -0300

    Visualizacion completa de semillas (tabla)
    Ya se pueden ver todas las semillas en la tabla con su paginación correspondiente, y también cada semilla por separado.
    Estilos al dia, solo queda terminar de maquetar "semilla.html"

--- src/main/resources/static/js/semilla.js
+++ src/main/resources/static/js/semilla.js
@@ -1,7 +1,20 @@
 const d = document;
 
-/* OBSERVACIONES GENERALES (SEMILLA) */
+/* OBSERVACIONES GENERALES */
 d.querySelector('.observaciones-menu').addEventListener("click", () => {
   d.querySelector('.observaciones-arrow').classList.toggle("active");
   d.querySelector('.observaciones-form-container').classList.toggle("active");
 });
+
+/* MODIFICAR ESTADO */
+d.querySelector('.modificar-estado-menu').addEventListener("click", () => {
+  d.querySelector('.modificar-estado-menu .arrow').classList.toggle("active");
+  d.querySelector('.modificar-estado-options').classList.toggle("active");
+});
+d.querySelectorAll(".modificar-estado-option").forEach((n) =>
+  n.addEventListener("click", () => {
+    alert('Estado modificado!');
+    d.querySelector('.modificar-estado-menu .arrow').classList.remove("active");
+    d.querySelector('.modificar-estado-options').classList.remove("active");
+  })
+);
