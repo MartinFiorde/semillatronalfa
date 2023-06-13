4b542e2362d98d2b66e380eca821d88fc56eafbd
JulianPepaDegani97 <julianpepadegani97@gmail.com>
2023-06-13 18:27:37 -0300

    Actualización de atributos en Event y EventDetails

--- src/main/java/ar/com/semillero/semillatronalfa/dtos/event/EventDto.java
+++ src/main/java/ar/com/semillero/semillatronalfa/dtos/event/EventDto.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Data
 @NoArgsConstructor
@@ -15,30 +16,40 @@ public class EventDto {
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate date;
     private String title;
+    private String image;
     private Boolean offeredBySemillero;
     private String status;
     private String organizedBy;
     private String type;
     private String approach;
     private String instructor;
+    private String description;
+    private String location;
     private String origin;
     private String destination;
     private int duration;
     private String modality;
+    private LocalTime startTime;
+    private LocalTime endingTime;
 
     public EventDto(Event event) {
         this.id = event.getId();
         this.date = event.getDate();
         this.title = event.getTitle();
+        this.image = event.getImage();
         this.offeredBySemillero = event.getOfferedBySemillero();
         this.status = event.getStatus();
         this.organizedBy = event.getOrganizedBy();
         this.type = event.getType();
         this.approach = event.getApproach();
         this.instructor = event.getDetails().getInstructor();
+        this.description = event.getDetails().getDescription();
+        this.location = event.getDetails().getLocation();
         this.origin = event.getDetails().getOrigin();
         this.destination = event.getDetails().getDestination();
         this.duration = event.getDetails().getDuration();
         this.modality = event.getDetails().getModality();
+        this.startTime = event.getDetails().getStartTime();
+        this.endingTime = event.getDetails().getEndingTime();
     }
 }
